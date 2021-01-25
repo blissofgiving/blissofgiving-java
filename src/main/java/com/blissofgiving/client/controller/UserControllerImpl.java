a7453cbbb55dbd8ecd122c8afd047e07501352b8
i831891 <shashidhar.koukuntla@sap.com>
2021-01-24 20:49:40 -0500

    Added COSR changes.

--- src/main/java/com/blissofgiving/client/controller/UserControllerImpl.java
+++ src/main/java/com/blissofgiving/client/controller/UserControllerImpl.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"http://blissofgiving-react-app.s3-website.us-east-2.amazonaws.com"})
 public class UserControllerImpl implements UserController {
 
     @Autowired
