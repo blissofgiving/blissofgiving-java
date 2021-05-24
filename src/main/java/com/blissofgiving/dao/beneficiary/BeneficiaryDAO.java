0d1bb48b36977530cc8b8a1dfd42d2b0f86d80f5
tribhuvon.tallapragada <tribhuvon.tallapragada@sap.com>
2021-05-24 17:30:32 -0400

    Beneficiary CURD

---
+++ src/main/java/com/blissofgiving/dao/beneficiary/BeneficiaryDAO.java
@@ -0,0 +1,8 @@
+package com.blissofgiving.dao.beneficiary;
+
+import com.blissofgiving.model.Beneficiary;
+
+public interface BeneficiaryDAO {
+
+	void createBeneficiary(Beneficiary beneficiary);
+}
